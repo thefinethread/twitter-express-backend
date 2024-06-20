946bc87d96ca1b2e71141c4bc3d752d2e5a002b7
thefinethread <kartikey8604@gmail.com>
2024-06-20 15:43:22 +0530

    added user model validation

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,6 +1,12 @@
+const Objection = require('objection');
+
 const errorMiddleware = (err, req, res, next) => {
 	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-	const message = err.message;
+	let message = err.message;
+
+	if (err instanceof Objection.ValidationError) {
+		message = validationError(err.data);
+	}
 
 	res.status(statusCode).json({
 		message,
@@ -9,6 +15,16 @@ const errorMiddleware = (err, req, res, next) => {
 	});
 
 	next();
+};
+
+const validationError = (data) => {
+	let errObj = {};
+
+	Object.keys(data).forEach((key) => {
+		errObj[key] = data[key][0].message;
+	});
+
+	return errObj;
 };
 
 module.exports = errorMiddleware;
--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,4 @@
-const { Model } = require('objection');
+const { Model, ValidationError } = require('objection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
@@ -6,12 +6,27 @@ class User extends Model {
 		return 'user';
 	}
 
-	// encrypt password
+	// JSON schema for validation
+	static get jsonSchema() {
+		return {
+			type: 'object',
+			required: ['name', 'email', 'password'],
+			properties: {
+				id: { type: 'string', format: 'uuid' },
+				name: { type: 'string', minLength: 1, maxLength: 255 },
+				email: { type: 'string', format: 'email' },
+				password: { type: 'string', minLength: 6 },
+			},
+		};
+	}
+
+	// Encrypt password before insert
 	async $beforeInsert() {
+		await super.$beforeInsert();
 		this.password = await bcrypt.hash(this.password, 10);
 	}
 
-	//verify hash password
+	// Verify hashed password
 	async validatePassword(enteredPassword) {
 		return await bcrypt.compare(enteredPassword, this.password);
 	}
